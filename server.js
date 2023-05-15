f2c8ec5a059deb0d1c17ba3fc3570c41203e6b2e
Yarden Katz <yrdnkz@gmail.com>
2023-05-15 23:01:21 +0300

    added ssl config

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const db = knex({
 		host: process.env.DB_HOST,
 		user: process.env.DB_NAME,
 		password: process.env.DB_PASSWORD,
-		database: process.env.DB_NAME
+		database: process.env.DB_NAME,
+		ssl: true
 	}
 })
 
